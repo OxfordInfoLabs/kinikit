a0fc4f85f05746870ce80442bb593622f5857ca5
Mark Robertshaw <mark@oxil.co.uk>
2019-10-16 21:46:48 +0100

    New composer JSON file

--- src/Views/page/home.php
+++ src/Views/page/home.php
@@ -1,7 +1,6 @@
 <h2>Welcome to the Home Controller</h2>
 <p>This is a minimal starter application ready for your development.</p>
 
-
 <p>Below are some results from an example database query using the ORM:</p>
 
 <table class="table striped">
